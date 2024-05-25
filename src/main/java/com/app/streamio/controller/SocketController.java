9a013f9cb18508b576fdf0d3d1d7b8c9701ea2b2
rafu01 <nrafsan13@gmail.com>
2024-05-25 17:02:23 +0600

    Apply changes to controller

--- src/main/java/com/app/streamio/controller/SocketController.java
+++ src/main/java/com/app/streamio/controller/SocketController.java
@@ -1,32 +1,13 @@
 package com.app.streamio.controller;
 
-import com.app.streamio.dto.HelloMessage;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import static com.app.streamio.utils.StaticConstants.INDEX_VIEW;
-
-@Slf4j
 @Controller
-@RequiredArgsConstructor
 public class SocketController {
-
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public String greet(HelloMessage helloMessage) {
-        log.info("message received: {}",helloMessage);
-        return "Received";
-    }
-
     @GetMapping("/home")
-    public String getHome(Model model) {
-        model.addAttribute("message", "hello world");
-        log.info("Here");
-        return INDEX_VIEW;
+    public String redirectToIndex() {
+        System.out.println("here");
+        return "index";
     }
 }
