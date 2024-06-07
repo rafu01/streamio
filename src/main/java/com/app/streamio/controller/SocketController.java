fc7e14ba20968f5c00b21e59bcb917ead95c044b
rafu01 <nrafsan13@gmail.com>
2024-06-07 22:18:05 +0600

    Redirect to static webpage

--- src/main/java/com/app/streamio/controller/SocketController.java
+++ src/main/java/com/app/streamio/controller/SocketController.java
@@ -2,10 +2,12 @@ package com.app.streamio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class SocketController {
-    @GetMapping("/home")
+    @GetMapping("/")
     public String redirectToIndex() {
         System.out.println("here");
         return "index";
